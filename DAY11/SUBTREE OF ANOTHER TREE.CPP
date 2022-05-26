1fac7cde403c27f11152ecf55965ed751a0730e9
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-05-26 23:26:42 +0530

    Add files via upload

---
+++ DAY11/SUBTREE OF ANOTHER TREE.CPP
@@ -0,0 +1,38 @@
+class Solution {
+    vector<TreeNode*> nodes;
+
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if (!s && !t) return true;
+        if (!s || !t) return false;
+
+        getDepth(s, getDepth(t, -1));
+
+        for (TreeNode* n: nodes)
+            if (identical(n, t))
+                return true;
+
+        return false;
+    }
+
+    int getDepth(TreeNode* r, int d) {
+        if (!r)
+            return -1;
+
+        int depth = max(getDepth(r->left, d), getDepth(r->right, d)) + 1;
+
+        // Check if depth equals required value
+        // Require depth is -1 for tree t (only return the depth, no push)
+        if (depth == d)
+            nodes.push_back(r);
+
+        return depth;
+    }
+
+    bool identical(TreeNode* a, TreeNode* b) {
+        if (!a && !b) return true;
+        if (!a || !b || a->val != b->val) return false;
+
+        return identical(a->left, b->left) && identical(a->right, b->right);
+    }
+};
