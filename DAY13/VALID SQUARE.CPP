34d58a3b531738928f851f0079590720998d8ee9
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-05-28 15:32:46 +0530

    Add files via upload

---
+++ DAY13/VALID SQUARE.CPP
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // helper function to calculate distance between two points
+    double dist(vector<int>& p1, vector<int>& p2) {
+        return sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]));
+    }
+    
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        // keep points in an array to be able to get them by index
+        vector<vector<int>> points = {p1, p2, p3, p4};
+        set<double> lengths;
+        
+        for (int i=0; i<4; i++) {
+            for (int j=i+1; j<4; j++) {
+                double curr = dist(points[i], points[j]);
+                if (curr != 0)
+                    lengths.insert(curr);
+                
+                // if distance is zero - we got a duplicated point
+                else
+                    return false;
+            }
+        }
+        // We are supposed to end with only two different lengths: the sides and the diagonals
+        return lengths.size() == 2;
+    }
+};
