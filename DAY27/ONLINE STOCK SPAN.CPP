197ae1f8a41267cb53303adf58ad6b496e86bb60
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-06-11 23:09:27 +0530

    Add files via upload

---
+++ DAY27/ONLINE STOCK SPAN.CPP
@@ -0,0 +1,13 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    int next(int price) {
+        int res = 1;
+        while (!s.empty() && s.top().first <= price) {
+            res += s.top().second;
+            s.pop();
+        }
+        s.push({price, res});
+        return res;
+    }
+};
