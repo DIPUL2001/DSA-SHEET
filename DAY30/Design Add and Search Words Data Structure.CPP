93d8bfb88e47ad9f50ee6cbb20476b92de9cb960
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-06-14 23:12:25 +0530

    Add files via upload

---
+++ DAY30/Design Add and Search Words Data Structure.CPP
@@ -0,0 +1,60 @@
+class TrieNode {
+public:
+    bool word;
+    TrieNode* children[26];
+    TrieNode() {
+        word = false;
+        memset(children, NULL, sizeof(children));
+    }
+};
+
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (!node -> children[c - 'a']) {
+                node -> children[c - 'a'] = new TrieNode();
+            }
+            node = node -> children[c - 'a'];
+        }
+        node -> word = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        return search(word.c_str(), root);
+    }
+private:
+    TrieNode* root = new TrieNode();
+    
+    bool search(const char* word, TrieNode* node) {
+        for (int i = 0; word[i] && node; i++) {
+            if (word[i] != '.') {
+                node = node -> children[word[i] - 'a'];
+            } else {
+                TrieNode* tmp = node;
+                for (int j = 0; j < 26; j++) {
+                    node = tmp -> children[j];
+                    if (search(word + i + 1, node)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return node && node -> word;
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
