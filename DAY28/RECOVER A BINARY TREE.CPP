cb390f202eb128f40e77f86ad50db4fb299f02fc
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-06-19 23:32:54 +0530

    Add files via upload

---
+++ DAY28/RECOVER A BINARY TREE.CPP
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    
+    void markTree(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
+        if (!root) return;
+        markTree(root->left, prev, first, end);
+        if (prev) {
+            if (root->val < prev->val) {
+                if (!first) {
+                    first = prev;
+                }
+                end = root;
+            }
+        }
+        prev = root;
+        markTree(root->right, prev, first, end);
+    }
+    
+    
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode *prev = nullptr, *first = nullptr, *end = nullptr;
+        markTree(root, prev, first, end);
+        swap(first->val, end->val);
+        return;
+    }
+};
