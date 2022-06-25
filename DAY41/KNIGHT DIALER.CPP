16d3671fd7f7e11ba5a3acef4df4afb345a4f4aa
DIPUL2001 <66951728+DIPUL2001@users.noreply.github.com>
2022-06-26 03:49:03 +0530

    Add files via upload

---
+++ DAY41/KNIGHT DIALER.CPP
@@ -0,0 +1,11 @@
+class Solution {
+public:
+   vector<vector<int>> ms = { {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {4, 2}};
+int knightDialer(int N) {
+  vector<int> d1(10, 1), d2(10);
+  for (; --N > 0; swap(d1, d2))
+    for (auto i = 0; i < 10; ++i) 
+        d2[i] = accumulate(begin(ms[i]), end(ms[i]), 0, [&](int s, int i) {return (s + d1[i]) % 1000000007;});
+  return accumulate(begin(d1), end(d1), 0, [](int s, int n) {return (s + n) % 1000000007;});
+}
+};
---
+++ DAY41/ONES AND ZEROS.CPP
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   int findMaxForm(vector<string>& strs, int m, int n) {
+  vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
+  int numZeroes, numOnes;
+
+  for (auto &s : strs) {
+    numZeroes = numOnes = 0;
+    // count number of zeroes and ones in current string
+    for (auto c : s) {
+      if (c == '0')
+	numZeroes++;
+      else if (c == '1')
+	numOnes++;
+    }
+
+    // memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
+    // from the first few strings up to the current string s
+    // Catch: have to go from bottom right to top left
+    // Why? If a cell in the memo is updated(because s is selected),
+    // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
+    // If we go from top left to bottom right, we would be using results from this iteration => overcounting
+    for (int i = m; i >= numZeroes; i--) {
+	for (int j = n; j >= numOnes; j--) {
+          memo[i][j] = max(memo[i][j], memo[i - numZeroes][j - numOnes] + 1);
+	}
+    }
+  }
+  return memo[m][n];
+}
+};
